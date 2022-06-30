105120ff7b148a66c2acfc6ca866604498a0152a
iamjoseph1998 <iamjoseph1998@gmail.com>
2022-06-30 18:10:44 +0530

    Created book model

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    book_name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.book_name
